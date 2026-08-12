79808ced31b16ccbc6aa1b46549aa39519e8fa89
Jimmy Al Ma <jimmyalma2121@gmail.com>
2026-08-12 18:17:37 +0700

    feat: add save file functionality with success notification

--- renderer.js
+++ renderer.js
@@ -3,11 +3,24 @@ const textArea = document.querySelector('#textArea')
 const openButton = document.querySelector('#openButton')
 const saveButton = document.querySelector('#saveButton')
 
+let totalCharacter = 0
+
 openButton.addEventListener('click', async () => {
     const response = await window.fileApi.openFile()
 
+    totalCharacter = response.content.length
+
     if (!response.canceled) {
         textArea.value = response.content
-        totalCharacterText.innerText = `Total Character: ${response.content.length}`
+        totalCharacterText.innerText = `Total Character: ${totalCharacter}`
+    }
+})
+
+saveButton.addEventListener('click', async () => {
+    const text = textArea.value
+    const response = await window.fileApi.saveFile(text)
+
+    if (response.success) {
+        alert('File saved successfully')
     }
 })
